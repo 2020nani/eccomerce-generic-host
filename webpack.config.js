b8d574f1076fbc50a30cd1d26fd8f97721704a39
2020nani <nanialmeida2019@gmail.com>
2024-08-11 16:26:38 -0300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const {
+  shareAll,
+  withModuleFederationPlugin,
+} = require("@angular-architects/module-federation/webpack");
+
+module.exports = withModuleFederationPlugin({
+  remotes: {
+    store: "http://localhost:4201/remoteEntry.js",
+  },
+
+  shared: {
+    ...shareAll({
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: "auto",
+    }),
+  },
+});
